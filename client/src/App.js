5585c042a664360f2eaa23ddfa48bbed1ac88e89
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-12-14 10:36:36 +0300

    Building the App component with material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import React from 'react'
+import { Container, Grid, Grow, AppBar, Typography } from '@material-ui/core';
+import moments from './images/moments.png';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Moments</Typography>
+        <img src={moments} alt="moments" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={4}>
+            <Grid item xs={12} sm={7}>
+
+            </Grid>
+            <Grid item xs={12} sm={5}>
+              
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container >
+  )
 }
 
 export default App;
