e6a20227b4a74ffdff7bfc3feaa8e8d1566911bb
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-12-14 09:28:31 +0300

    Create a model and a controller for the app

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,7 +17,7 @@ const CONNECTION_URL = 'mongodb+srv://ejoka:Tanzania99!!@cluster0.wqald.mongodb.
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => `Server is Running on port: ${PORT}`))
+  .then(() => app.listen(PORT, () => console.log(`Server is Running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
