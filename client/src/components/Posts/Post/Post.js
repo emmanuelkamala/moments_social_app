34216edc9ca6da2d26c7f6aa9861cd074906e452
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-01-18 17:13:04 +0300

    Add styling items in Post component

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Card, CardContent, CardMedia, CardActions, Button, Typography} from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/core/icons/ThumbUpAlt';
 import useStyles from './styles';
 
 function Post() {
