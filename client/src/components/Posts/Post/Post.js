8fb2998d266c4c5a6d90d3df19ffddbc65bd99ce
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-01-18 18:59:30 +0300

    Add material-ui icons to post component

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Card, CardContent, CardMedia, CardActions, Button, Typography} from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/core/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/core/icons/Delete';
+import MoreHorizIcon from '@material-ui/core/icons/MoreHoriz';
+import moment from 'moment';
 import useStyles from './styles';
 
-function Post() {
+function Post({ post }) {
   const classes = useStyles();
   
   return (
-    <div>
-      <h2>My post</h2>
-    </div>
+    <Card className={classes.card} >
+      <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+    </Card>
   )
 }
 
