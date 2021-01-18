2de91e073b9d99b5025c520dab9d21a888de0940
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-01-18 19:53:06 +0300

    Install material-ui icons

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Card, CardContent, CardMedia, CardActions, Button, Typography} from '@material-ui/core';
-import ThumbUpAltIcon from '@material-ui/core/icons/ThumbUpAlt';
-import DeleteIcon from '@material-ui/core/icons/Delete';
-import MoreHorizIcon from '@material-ui/core/icons/MoreHoriz';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
 
@@ -15,6 +15,27 @@ function Post({ post }) {
       <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{color: 'white'}} size="small" onclick={()=>{}}>
+          <MoreHorizIcon fontSize="default" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">{post.tags.map(tag => `#${tag} `)}</Typography>
+        <CardContent>
+          <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+        </CardContent>
+        <CardActions className={classes.cardActions}>
+          <Button size="small" color="primary" onClick={()=>{}}>
+            <ThumbUpAltIcon fontSize="small" />
+            Like {post.likeCount}
+          </Button>
+          <Button size="small" color="primary" onClick={()=>{}}>
+            <DeleteIcon fontSize="small" />
+             Delete
+          </Button>
+        </CardActions>
       </div>
     </Card>
   )
